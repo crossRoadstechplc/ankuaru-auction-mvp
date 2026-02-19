4342a5a74dbebb043b3c26cfd6a2c201ee0062c2
samuel mulu <samuel-mulu>
2026-02-19 10:37:24 +0300

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Ankuaru - Premium Coffee Auctions",
+  description: "Specialty Hub for B2B Coffee Auctions",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="light">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className={`${inter.variable} font-display antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
